efef2a60e157d2663d78ad18822150988ee32934
WT <wangte168@sina.com>
2014-08-31 23:52:08 +0800

    优化

--- application/views/get.php
+++ application/views/get.php
@@ -10,7 +10,7 @@
 
 <body>
 <div style="margin-top: 50px;font-size:16px;text-align:center">
-	<a href="http://eco.te168.cn/static/eco.apk" style="margin:0 auto;color:#FFF; display: block; text-decoration:none;text-align:center; width: 80%; line-height: 40px; background: #3fb58f;">生态蔬菜APP点击下载</a>
+	<a href="http://eco.te168.cn/static/eco.apk" style="margin:0 auto;color:#FFF; display: block; text-decoration:none;text-align:center; width: 80%; line-height: 40px; background: #3fb58f;">生态蔬菜APP下载</a>
 	
 	<br />
 	<div>如采用微信无法下载，请使用右上角功能用浏览器打开</div>
